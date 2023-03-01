1d971e7235ee5b234d2ba01cb5f2d733fc010c92
jmarcossss <joaomalcantaraaa@gmail.com>
2023-03-01 12:01:24 -0300

    chore: first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+// import routers from "./routers/rotas.js";
+
+dotenv.config();
+
+const server = express();
+const PORTA = 5000;
+
+server.use(cors());
+server.use(json());
+
+// server.use(routers);
+
+server.listen(PORTA, () => {
+    console.log("Servidor rodando na porta 5000...")
+});
