c56aa66b873a3175973f809024c7290188c29984
jmarcossss <joaomalcantaraaa@gmail.com>
2023-03-07 00:22:15 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ import cors from "cors"
 import routers from "./routers/rotas.js";
 
 const server = express()
-const PORT= 5000
+const port = process.env.PORT || 5000;
+// const PORT= 5000
 
 server.use(cors())
 server.use(express.json())
 server.use(routers);
 
-server.listen(PORT, () => {
-    console.log("Servidor rodando na porta 5000...")
+// server.listen(PORT, () => {
+//     console.log("Servidor rodando na porta 5000...")
+// });
+server.listen(port, () => {
+    console.log(`Server running in port: ${port}`)
 });
